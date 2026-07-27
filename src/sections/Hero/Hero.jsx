2b1505698a93b86fe8c9c341fca1ac6798210c2f
John Regan <johnathan.regan.dev@gmail.com>
2026-07-26 22:52:08 -0400

    Remove resume button

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -15,13 +15,7 @@ function Hero({name, title, introduction}) {
                 <div className="hero__actions">
                     <Button href="#contact" variant="primary">Contact Me</Button>
 
-                    <Button
-                            href={`${import.meta.env.BASE_URL}Resume.pdf`}
-                            target="_blank"
-                            rel="noopener noreferrer"
->
-                            Resume
-                    </Button>
+                    
                 </div>
             </div>
 
